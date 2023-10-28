3b5ade6e993eed29804254e434d424fd1933341b
SabahatSyed <sabahatsyed2000@gmail.com>
2023-10-28 14:05:51 +0500

    forgot password & hide and show password

--- src/app/resetpassword/page.jsx
+++ src/app/resetpassword/page.jsx
@@ -2,12 +2,15 @@ import Image from 'next/image'
 import Reset from './reset'
 import Partners from '@/app/partners/partners'
 import Footer from '@/components/footer'
+import { getCustomers } from '@/api/shopifyapis'
 
 export default async function ResetPass() {
+    const customers = await getCustomers()
+
   return (
     <div className='h-screen '>
-      <Reset />
-      <Footer activePage={'login'} />
+      <Reset customers={customers} />
+      <Footer activePage={'login'}  />
     </div>
   )
 }
--- src/app/resetpassword/reset.jsx
+++ src/app/resetpassword/reset.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Link from "next/link";
 import { useRouter } from "next-nprogress-bar";
 import Image from "next/image";
@@ -9,6 +9,7 @@ import Loader from "@/components/Loader";
 import { gql, useMutation } from "@apollo/client";
 import { useSuspenseQuery } from "@apollo/experimental-nextjs-app-support/ssr";
 import { useSearchParams } from "next/navigation";
+import { updateCustomer } from "@/api/shopifyapis";
 Amplify.configure(awsconfig);
 const getlogin = gql`
   mutation SignInWithEmailAndPassword($email: String!, $password: String!) {
@@ -24,12 +25,16 @@ const getlogin = gql`
     }
   }
 `;
-export default function Reset({ data }) {
+export default function Reset({ length = 6, customers }) {
   const query = useSearchParams().get("email");
-  console.log(query);
   const history = useRouter();
-  const [OTP, setOTP] = useState("");
+  const [hide, sethide] = useState(false);
+  const [hide1, sethide1] = useState(false);
+
   const [msg, setmsg] = useState("");
+  const [passmsg, setPassmsg] = useState("");
+  const [validmsg, setValidmsg] = useState("");
+
   const [password, setPassword] = useState("");
   const [confirmpassword, setConfirmPassword] = useState("");
 
@@ -40,13 +45,14 @@ export default function Reset({ data }) {
     localStorage.setItem("customeraccesstoken", token);
   }, [token]);
 
- 
-   const [otp, setOtp] = useState(Array(length).fill(''));
-  const inputRefs = Array(length).fill(0).map((_, index) => useRef(null));
+  const [otp, setOtp] = useState(Array(length).fill(""));
+  const inputRefs = Array(length)
+    .fill(0)
+    .map((_, index) => useRef(null));
 
   const handleChange = (e, index) => {
     const value = e.target.value;
-    setOtp(prevOtp => {
+    setOtp((prevOtp) => {
       const newOtp = [...prevOtp];
       newOtp[index] = value;
 
@@ -61,34 +67,66 @@ export default function Reset({ data }) {
 
   const handleKeyDown = (e, index) => {
     // Move to the previous input field on backspace
-    if (e.key === 'Backspace' && index > 0 && !otp[index]) {
+    if (e.key === "Backspace" && index > 0 && !otp[index]) {
       inputRefs[index - 1].current.focus();
     }
   };
 
- const resendPassword = async () => {
-   setLoading(true);
-   try {
-     setmsg("");
-     await Amplify.Auth.forgotPassword(query);
-     setmsg("Password reset instructions sent successfully");
-     setLoading(false);
-   } catch (error) {
-     setmsg("Error sending password reset instructions", error);
-     setLoading(false);
-   }
- };
+  const resetPassword = async () => {
+    setLoading(true);
+    try {
+      setmsg("");
+      console.log("otp", otp.join(""));
+      await Amplify.Auth.forgotPasswordSubmit(query, otp.join(""), password);
+      const customer = customers?.customers.find((item) => item.email == query);
+      const res = updateCustomer(customer.id, {
+        customer: {
+          id: customer.id,
+          password: password,
+          password_confirmation: password,
+        },
+      });
+      if (res) {
+        setmsg("Password Reset Successfully!");
+        setLoading(false);
+        history.push("/login");
+      } else setmsg("Reset Password Failed");
+    } catch (error) {
+      console.log(error);
+      setmsg("Error Reseting Password", error);
+      setLoading(false);
+    }
+  };
+  const isValidPassword = (password) => {
+    const passwordRegex =
+      /^(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])(?=.*[A-Z]).{8,}$/;
+
+    return passwordRegex.test(password);
+  };
+
+  const resendPassword = async () => {
+    setLoading(true);
+    try {
+      setmsg("");
+      await Amplify.Auth.forgotPassword(query);
+      setmsg("Password reset instructions sent successfully");
+      setLoading(false);
+    } catch (error) {
+      setmsg("Error sending password reset instructions", error);
+      setLoading(false);
+    }
+  };
   return (
     <div className="bg-[url('/auth/bg.svg')] py-20 font-roboto">
-      <div className=" w-11/12 md:w-3/5 lg:w-2/5 rounded-3xl px-6 py-10 md:p-16 bg-white mx-auto flex flex-col gap-7 text-black">
+      <div className=" w-11/12 md:w-3/5 lg:w-2/5 rounded-3xl px-6 py-10 md:p-16 bg-white mx-auto flex flex-col gap-5 text-black">
         <img src="/Home/logo.svg" className="h-28 w-28 mx-auto" />
         <p className="font-bold text-[28px] text-center">OTP Verification</p>
         <p className="text-sm text-center w-80">
           Enter the 6-digit code sent to your registered email address to verify
           your identity. If you didn't receive the code, click 'Resend Code'.
         </p>
-        <div className="gap-2 flex flex-col">
-          <div className="otp-input-container">
+        <div className="gap-2 grid grid-cols-8 bg-formbg rounded-md py-2">
+          <div className="col-start-2 col-span-6 grid grid-cols-6 gap-2">
             {otp.map((digit, index) => (
               <input
                 key={index}
@@ -98,50 +136,114 @@ export default function Reset({ data }) {
                 onKeyDown={(e) => handleKeyDown(e, index)}
                 maxLength="1"
                 ref={inputRefs[index]}
-                className="otp-input"
+                className="font-bold text-3xl text-center text-black border-b-2 border-b-underlinegray bg-transparent focus:border-b-2 outline-none"
               />
             ))}
           </div>
         </div>
+        <p
+          onClick={() => {
+            resendPassword();
+          }}
+          className="text-bgblue hover:text-black cursor-pointer text-sm font-medium text-center">
+          Resend Code
+        </p>
         <div className="gap-2 flex flex-col">
           <label className="text-xs font-bold">Password</label>
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            placeholder={"*******"}
-            label={"Password"}
-            className="h-8 p-6 bg-formbg text-formtext rounded-md"
-          />
-        </div>{" "}
+          <div className="h-8 p-6 bg-formbg text-formtext rounded-md flex justify-between items-center">
+            <input
+              onChange={(e) => {
+                setPassword(e.target.value);
+                const a = isValidPassword(e.target.value);
+                console.log(a);
+                if (!a)
+                  setValidmsg(
+                    "Password must have at least 8 characters,numbers,special characters,upper case letters"
+                  );
+                else setValidmsg("");
+              }}
+              value={password}
+              placeholder={"*******"}
+              label={"Password"}
+              type={hide ? "password" : "text"}
+              className="bg-formbg outline-none "
+            />
+
+            <img
+              onClick={() => sethide(!hide)}
+              className="cursor-pointer"
+              src="/auth/eye-off.svg"
+            />
+          </div>
+        </div>
+        {validmsg && password != "" && (
+          <p className={`text-xxs font-bold text-red-600`}>{validmsg}</p>
+        )}
         <div className="gap-2 flex flex-col">
           <label className="text-xs font-bold">Confirm Password</label>
-          <input
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            value={confirmpassword}
-            placeholder={"******"}
-            label={"Confirm Password"}
-            className="h-8 p-6 bg-formbg text-formtext rounded-md"
-          />
+          <div className="h-8 p-6 bg-formbg text-formtext rounded-md flex justify-between items-center">
+            <input
+              onChange={(e) => {
+                setConfirmPassword(e.target.value);
+                if (e.target.value != password) {
+                  setPassmsg("Passwords donot Match!");
+                } else {
+                  setPassmsg("Passwords Match!");
+                }
+              }}
+              value={confirmpassword}
+              placeholder={"******"}
+              label={"Confirm Password"}
+              type={hide ? "password" : "text"}
+              className="bg-formbg outline-none "
+            />
+            <img
+              onClick={() => sethide(!hide)}
+              className="cursor-pointer"
+              src="/auth/eye-off.svg"
+            />
+          </div>
         </div>
+        {passmsg && (
+          <p
+            className={`text-sm font-bold ${
+              passmsg.includes("donot") ? "text-red-600" : "text-green-700"
+            }`}>
+            {passmsg}
+          </p>
+        )}
         {msg && (
           <p
             className={`text-sm font-bold ${
-              msg.includes("successfully") ? "text-green-700" : "text-red-500"
+              msg.includes("Successfully") ? "text-green-700" : "text-red-500"
             }`}>
             {msg}
           </p>
         )}
         <button
-          onClick={() => forgotPassword()}
-          className="bg-bgblue text-white uppercase p-4 rounded-md flex justify-center items-center"
-          disabled={loading}>
+          onClick={() => {
+            if (!passmsg.includes("donot")) {
+              console.log("hello");
+              resetPassword();
+              setPassmsg("");
+            }
+          }}
+          className={`${
+            loading ||
+            passmsg.includes("donot") ||
+            password == "" ||
+            confirmpassword == ""
+              ? "bg-slate-600"
+              : "bg-bgblue"
+          } text-white uppercase p-4 rounded-md flex justify-center items-center`}
+          disabled={loading || passmsg.includes("donot")}>
           {loading ? (
             <>
               <Loader />
               Sending Confirmation Code...
             </>
           ) : (
-            <>Send Confirmation Code</>
+            <>Reset Password</>
           )}
         </button>
       </div>
